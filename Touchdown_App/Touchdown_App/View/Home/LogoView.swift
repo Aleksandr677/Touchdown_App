5e3566c56ec60dc7b42ae8b9c61f26468c1e27e4
Aleksandr677 <hristicenkoaleksandr@MacBook-Air-Hristicenko.local>
2022-10-30 13:24:59 +0300

    feat: add navigationBarView, LogoView

---
+++ Touchdown_App/Touchdown_App/View/Home/LogoView.swift
@@ -0,0 +1,41 @@
+//
+//  LogoView.swift
+//  Touchdown_App
+//
+//  Created by Христиченко Александр on 2022-10-30.
+//
+
+import SwiftUI
+
+struct LogoView: View {
+    //MARK: - PROPERIES
+    
+    //MARK: - BODY
+    var body: some View {
+        HStack(spacing: 4) {
+            Text("Touch".uppercased())
+                .font(.title3)
+                .fontWeight(.black)
+                .foregroundColor(.black)
+            
+            Image("logo-dark")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 30, height: 30, alignment: .center)
+            
+            Text("Down".uppercased())
+                .font(.title3)
+                .fontWeight(.black)
+                .foregroundColor(.black)
+        } //HStack
+    }
+}
+
+//MARK: - PREVIEW
+struct LogoView_Previews: PreviewProvider {
+    static var previews: some View {
+        LogoView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
